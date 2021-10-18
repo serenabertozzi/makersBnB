1019adab2281a9696853aeac6d3c415bb7ffab0a
ConorButler <cwdbutler@gmail.com>
2021-10-18 12:11:14 +0100

    first commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+ruby '3.0.0'
+
+gem 'rake'
+gem 'pg'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'sinatra-flash'
+gem 'bcrypt'
+# gem launchy
+
+group :test do
+  gem 'capybara', group: :test
+  gem 'rspec'
+  gem 'timecop'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
+
+group :development, :test do
+  gem 'rubocop', '1.20'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,33 @@
+ENV["RACK_ENV"] = "test"
+ENV["ENVIRONMENT"] = "test"
+
+require File.join(File.dirname(__FILE__), "..", "app.rb")
+
+require 'rake'
+Rake.application.load_rakefile
+require 'setup_connection'
+
+require 'simplecov'
+require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+
+Capybara.app = Chitter
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.before(:each) do 
+    Rake::Task['setup_test_database'].execute
+  end
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
+end
