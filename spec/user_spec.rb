8b372cf5af1203aeb92a1fb1ec1b0d52efd1e07d
Rob <rob.eagleheart@gmail.com>
2021-10-19 14:07:29 +0100

    host change

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,15 +5,15 @@ describe User do
 
      describe '.create' do
       it 'does not create a new user if the email is taken' do
-        User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host_id: true, password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
-        user = User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host_id: true, password: '123457jghjhgj', password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
+        User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host: true, password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
+        user = User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host: true, password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
         expect(user).to be_falsy
       end
     end
   
     describe '.find' do
       it 'returns the requested user object' do
-        user = User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host_id: true, password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
+        user = User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host: true, password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
   
         result = User.find(id: user.id)
         persisted_data = persisted_data(table: 'users', id: user.id )
@@ -24,18 +24,18 @@ describe User do
         expect(result.email).to eq persisted_data['email']
         expect(result.first_name).to eq persisted_data['first_name']
         expect(result.last_name).to eq persisted_data['last_name']
-        expect(result.host_id).to eq persisted_data['host_id']
+        expect(result.host).to eq persisted_data['host']
         expect(true).to eq (BCrypt::Password.new(persisted_data['password']) == '123457jghjhgj')
         expect(result.email).to eq 'test@test.com'
         expect(result.first_name).to eq 'Name'
         expect(result.last_name).to eq 'Surname'
-        expect(result.host_id).to eq "t"
+        expect(result.host).to eq "t"
       end
     end
 
     describe '.log_in' do
         it 'returns the requested user object on log in' do
-          user = User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host_id: true, password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
+          user = User.create(email: 'test@test.com', first_name: 'Name', last_name: 'Surname', host: true, password: '123457jghjhgj', password_confirmation: '123457jghjhgj')
     
           result = User.log_in(email: 'test@test.com', password: '123457jghjhgj')
           persisted_data = persisted_data(table: 'users', id: user.id )
@@ -46,12 +46,12 @@ describe User do
           expect(result.email).to eq persisted_data['email']
           expect(result.first_name).to eq persisted_data['first_name']
           expect(result.last_name).to eq persisted_data['last_name']
-          expect(result.host_id).to eq persisted_data['host_id']
+          expect(result.host).to eq persisted_data['host']
           expect(true).to eq (BCrypt::Password.new(persisted_data['password']) == '123457jghjhgj')
           expect(result.email).to eq 'test@test.com'
           expect(result.first_name).to eq 'Name'
           expect(result.last_name).to eq 'Surname'
-          expect(result.host_id).to eq "t"
+          expect(result.host).to eq "t"
         end
     end
 
