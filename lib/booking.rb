e94e11be44debe9cacd9ede0da6c2352c8a2f85a
ConorButler <cwdbutler@gmail.com>
2021-10-19 11:02:04 +0100

    Fixed booking model

--- lib/booking.rb
+++ lib/booking.rb
@@ -9,25 +9,32 @@ class Booking
     @start_date = start_date
     @end_date = end_date
     @bnb_id = bnb_id
-    @usr_id = user_id
+    @user_id = user_id
   end
 
   def self.create(start_date:, end_date:, bnb_id:, user_id:)
     result = DatabaseConnection.query(
-      "INSERT INTO bookings(start_date, end_date, bnb_id, user_id) VALUES($1, $2, $3, $4) RETURNING *;",
+      "INSERT INTO bookings(start_date, end_date, bnb_id, user_id)
+      VALUES($1, $2, $3, $4) RETURNING *;",
       [start_date, end_date, bnb_id, user_id]
     )
-    
-    booking.new(id: result[0]['id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'],
-    bnb_id: result[0]['bnb_id'], user_id: result[0]['user_id'])
+
+    Booking.new(
+      id: result[0]['id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'],
+      bnb_id: result[0]['bnb_id'], user_id: result[0]['user_id']
+    )
   end
 
   def self.all
-    result = DatabaseConnection.query("SELECT * FROM bookings;")
+    result = DatabaseConnection.query(
+      "SELECT * FROM bookings;"
+    )
 
     result.map do |booking| 
-      Booking.new(id: booking['id'], start_date: booking['start_date'], end_date: booking['end_date'],
-      bnb_id: booking['bnb_id'], user_id: booking['user_id'])
+      Booking.new(
+        id: result[0]['id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'],
+        bnb_id: result[0]['bnb_id'], user_id: result[0]['user_id']
+      )
     end.sort_by { |booking| booking.start_date }.reverse
   end
 
@@ -37,16 +44,35 @@ class Booking
     )
     return unless result.any?
 
-    booking.new(id: result[0]['id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'],
-    bnb_id: result[0]['bnb_id'], user_id: result[0]['user_id'])
+    Booking.new(
+      id: result[0]['id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'],
+      bnb_id: result[0]['bnb_id'], user_id: result[0]['user_id']
+    )
   end
 
-  update
+  def self.update(id:, start_date:, end_date:)
+    result = DatabaseConnection.query(
+      "UPDATE bookings SET start_date = $1, end_date = $2,
+      WHERE id = $3 RETURNING *;", 
+      [start_date, end_date, id]
+    )
 
-  delete
+    Booking.new(
+      id: result[0]['id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'],
+      bnb_id: result[0]['bnb_id'], user_id: result[0]['user_id']
+    )
+  end
 
+  def self.delete(id:)
+    result = DatabaseConnection.query(
+      "DELETE FROM bookings WHERE id = $1;", [id]
+    )
 
-
+    Booking.new(
+      id: result[0]['id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'],
+      bnb_id: result[0]['bnb_id'], user_id: result[0]['user_id']
+    )
+  end
 
 end
 
