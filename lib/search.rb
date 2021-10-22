7300c46e676a3348b7d1a5821aef2de4e93d4672
ConorButler <cwdbutler@gmail.com>
2021-10-22 10:09:21 +0100

    Search fixed for real this time :)

--- lib/search.rb
+++ lib/search.rb
@@ -20,46 +20,33 @@ class Search
     max_price = '10000' if max_price == "" || max_price.nil?
     start_date = Time.new(1900) if start_date == "" || start_date.nil?
     end_date = Time.new(1900) if end_date == "" || end_date.nil?
-    p "INSIDE FILTER"
-    p location
-    p min_price
-    p max_price
-    p start_date
-    p end_date
+ 
     unless location.empty?
       results = DatabaseConnection.query(
-        "SELECT bnbs.id, bnbs.name, bnbs.location, bnbs.price, bnbs.user_id, bookings.start_date, bookings.end_date
+        "SELECT id, name, location, price, user_id
         FROM bnbs
-        LEFT JOIN bookings
-        ON bnbs.id = bookings.bnb_id
         WHERE LOWER(bnbs.location) = $1
         AND price BETWEEN $2 AND $3
-        AND (bookings.start_date IS NULL
-          OR bookings.start_date NOT BETWEEN $4 AND $5)
-        AND (bookings.end_date IS NULL
-          OR bookings.end_date NOT BETWEEN $4 AND $5)
-        ;", [location.downcase, min_price, max_price, start_date, end_date]
+        ;", [location.downcase, min_price, max_price]
       )
     else
       results = DatabaseConnection.query(
-        "SELECT bnbs.id, bnbs.name, bnbs.location, bnbs.price, bnbs.user_id, bookings.start_date, bookings.end_date
+        "SELECT id, name, location, price, user_id
         FROM bnbs
-        LEFT JOIN bookings
-        ON bnbs.id = bookings.bnb_id
         WHERE price BETWEEN $1 AND $2
-        AND (bookings.start_date IS NULL
-          OR bookings.start_date NOT BETWEEN $3 AND $4)
-        AND (bookings.end_date IS NULL
-          OR bookings.end_date NOT BETWEEN $3 AND $4)
-        ;", [min_price, max_price, start_date, end_date]
+        ;", [min_price, max_price]
       )
     end
     return unless results.any?
-    results.map do |result|
+    bnbs = results.map do |result|
       Search.new(
         bnb_id: result['id'], name: result['name'], location: result['location'],
         price: result['price'], user_id: result['user_id']
       )
+    end
+
+    bnbs.select do |bnb|
+      Bnb.available?(bnb_id: bnb.bnb_id, start_date: start_date, end_date: end_date)
     end
   end
 end
