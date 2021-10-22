9433b3fcfac2f0575863f0af493419f0bb01482c
Rob <rob.eagleheart@gmail.com>
2021-10-22 13:55:09 +0100

    fix

--- app.rb
+++ app.rb
@@ -35,7 +35,6 @@ class Makersbnb < Sinatra::Base
   end
 
   get "/search" do
-    p @bnbs
     @bnbs = Search.filter(
       location: params[:location], min_price: params[:min_price],
       max_price: params[:max_price], start_date: params[:start_date], end_date: params[:end_date],
